e839b186ad987a213b990aa75e4748ad145a3b86
foxjojo <35793308+foxjojo@users.noreply.github.com>
2022-11-03 11:28:56 +0800

    添加三种命名方式 添加一个图标

--- Flow.Launcher.Plugin.VarTranslation/Main.cs
+++ Flow.Launcher.Plugin.VarTranslation/Main.cs
@@ -5,16 +5,19 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
 using Newtonsoft.Json;
+using System.Globalization;
+using System.Text.RegularExpressions;
 
 namespace Flow.Launcher.Plugin.VarTranslation
 {
+    //CompletePreform
     public class Main : IAsyncPlugin
     {
         private PluginInitContext _context;
 
         public Task<List<Result>> QueryAsync(Query query, CancellationToken token)
         {
-            return GetData(query.Search);
+            return GetData(query.FirstSearch);
         }
 
         public Task InitAsync(PluginInitContext context)
@@ -29,29 +32,75 @@ namespace Flow.Launcher.Plugin.VarTranslation
 
         private async Task<List<Result>> GetData(string str)
         {
+            if(string.IsNullOrWhiteSpace(str))
+                return new List<Result>();
+
             List<Result> results = new List<Result>();
             var url = new StringBuilder();
             url.Append("https://v.api.aa1.cn/api/api-fanyi-yd/index.php?msg=");
             url.Append(str);
             url.Append("&type=1");
-            _context.API.ShowMsg(url.ToString());
             var json = await _context.API.HttpGetStringAsync(url.ToString());
-            _context.API.ShowMsg("定制", json);
-            var jsonData = JsonConvert.DeserializeObject<JsonData>(json);
-            _context.API.ShowMsg("翻译结果" + jsonData.text);
-            results.Add(new Result()
+            if (string.IsNullOrWhiteSpace(json))
+                return new List<Result> { };
+            try
             {
-                Title = jsonData.text,
-                SubTitle = jsonData.text,
-                Action = delegate (ActionContext context)
+                var jsonData = JsonConvert.DeserializeObject<JsonData>(json);
+
+                if (jsonData == null)
+                    return new List<Result> { };
+
+                var varArray = jsonData.text.Split(' ');
+                TextInfo myTI = new CultureInfo("en-US", false).TextInfo;
+
+                string snakeCase = myTI.ToLower(jsonData.text).Replace(' ', '_');
+                string camelCaseB =Regex.Replace( myTI.ToTitleCase(jsonData.text),@"\s","");
+                string camelCaseS = camelCaseB.Substring(0, 1).ToLower() + camelCaseB.Substring(1);
+
+                results.Add(new Result()
                 {
-                    _context.API.CopyToClipboard(jsonData.text);
-                    return true;
-                }
-            });
+                    Title = snakeCase,
+                    SubTitle = "下划线命名",
+                    IcoPath ="icon.png",
+                    Action = delegate (ActionContext context)
+                    {
+                        _context.API.CopyToClipboard(snakeCase);
+                        return true;
+                    }
+                }) ;
+                results.Add(new Result()
+                {
+                    Title = camelCaseB,
+                    SubTitle = "帕斯卡命名",
+                    IcoPath = "icon.png",
+                    Action = delegate (ActionContext context)
+                    {
+                        _context.API.CopyToClipboard(camelCaseB);
+                        return true;
+                    }
+                });
+                results.Add(new Result()
+                {
+                    Title = camelCaseS,
+                    SubTitle ="驼峰命名",
+                    IcoPath = "icon.png",
+                    Action = delegate (ActionContext context)
+                    {
+                        _context.API.CopyToClipboard(camelCaseS);
+                        return true;
+                    }
+                });
 
 
-            return results;
+                return results;
+            }
+            catch (Exception)
+            {
+                _context.API.LogInfo("VarTranslation", string.Format("需要查询的str:{0}", str));
+                _context.API.LogInfo("VarTranslation", string.Format("无法格式化json:{0}",json));
+               return new List<Result>();
+            }
+          
         }
 
         public class JsonData
